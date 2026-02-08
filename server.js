80d736fc37abd7251cb08d7bb66f656d08bb0ca9
rscavini <rscavini@scavinibcn.com>
2026-02-08 02:20:48 +0100

    Fix Express 5 wildcard route syntax for SPA routing

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Handle SPA routing: return index.html for all non-static paths
-app.get('*', (req, res) => {
+app.get('(.*)', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
